b665535cfeadb2e63ad28f1268e8386ef6424b19
Tomas Scerbak <tomas.scerbak@gmail.com>
2025-02-25 23:19:30 +0100

    basic typography setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Test</div>;
+  const [theme, setTheme] = useState("dark");
+  const [fontFamily, setFontFamily] = useState("sans-serif");
+
+  const handleTheme = () => {
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+
+  const handleFonts = () => {
+    setFontFamily((prev) =>
+      prev === "sans-serif" ? "monospace" : "sans-serif"
+    );
+  };
+
+  document.querySelector("body").setAttribute("color-theme", theme);
+  document.querySelector("body").setAttribute("font-family", fontFamily);
+
+  return (
+    <div>
+      <button onClick={handleTheme}>{theme}</button>
+      <button onClick={handleFonts}>{fontFamily}</button>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
+        repellat quod magnam sit possimus quibusdam eius consequuntur excepturi
+        iusto? Laudantium voluptates voluptatum voluptate eveniet corrupti
+        reiciendis quasi reprehenderit, quos doloremque!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
