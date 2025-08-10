8902ca5cf450dfe68b50780f31213742ad5d878d
Tomas Scerbak <tomas.scerbak@gmail.com>
2025-08-10 18:31:15 +0200

    add logout functionality with popup

--- client/src/components/Setting.jsx
+++ client/src/components/Setting.jsx
@@ -1,10 +1,9 @@
 import classes from "./Setting.module.css";
 
-const Setting = ({ img, text, borderTop = "" }) => {
+const Setting = ({ img, text, borderTop = "", onClick }) => {
   const containerClasses = `${classes.setting__container} ${borderTop ? classes[borderTop] : ""}`;
-  console.log(containerClasses);
   return (
-    <div className={containerClasses}>
+    <div onClick={onClick} className={containerClasses}>
       <img className={classes.setting__image} src={img} alt={text} />
       <p className={classes.setting__text}>{text}</p>
     </div>
--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+import { useAuth } from "../contexts/authContext";
+
 import Setting from "../components/Setting";
+import CallToActionModal from "../components/modals/CallToActionModal";
 
 import SunIcon from "../assets/icon-sun.svg";
 import FontIcon from "../assets/icon-font.svg";
@@ -6,6 +10,9 @@ import LogoutIcon from "../assets/icon-logout.svg";
 import LockIcon from "../assets/icon-lock.svg";
 
 const Settings = () => {
+  const { signOut } = useAuth();
+  const [showModal, setShowModal] = useState(false);
+
   const settings = [
     { name: "Color Theme", image: SunIcon },
     { name: "Font Theme", image: FontIcon },
@@ -13,18 +20,40 @@ const Settings = () => {
     { name: "Logout", image: LogoutIcon },
   ];
 
+  const handleSettingClick = (setting) => {
+    if (setting.name === "Logout") setShowModal(true);
+  };
+
   return (
-    <div>
-      <h1>Settings</h1>
-      {settings.map((setting, index) => (
-        <Setting
-          key={index}
-          img={setting.image}
-          text={setting.name}
-          borderTop={setting.name === "Logout" ? "borderTop" : ""}
+    <>
+      <div>
+        <h1>Settings</h1>
+        {settings.map((setting, index) => (
+          <Setting
+            key={index}
+            img={setting.image}
+            text={setting.name}
+            borderTop={setting.name === "Logout" ? "borderTop" : ""}
+            onClick={() => handleSettingClick(setting)}
+          />
+        ))}
+      </div>
+      {showModal && (
+        <CallToActionModal
+          header="Leaving so soon?"
+          message="Logging out will end your current session. You can always come back later. Want to continue or stay a bit longer?"
+          btnsArr={[
+            { title: "Cancel", variant: "cancel", onClick: () => setShowModal(false) },
+            {
+              title: "Logout",
+              variant: "delete",
+              onClick: () => signOut(),
+            },
+          ]}
+          image={LogoutIcon}
         />
-      ))}
-    </div>
+      )}
+    </>
   );
 };
 
