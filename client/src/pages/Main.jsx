2c355b7223b8edc339b51f5eeedf34d4cb0b6252
Tomas Scerbak <tomas.scerbak@gmail.com>
2025-08-31 19:50:42 +0200

    add flex to main component

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -5,7 +5,7 @@ import Attribution from "../components/Attribution";
 const Main = ({ children }) => {
   const location = useLocation();
   return (
-    <main style={{ height: "100dvh", overflow: "hidden" }}>
+    <main style={{ height: "100dvh", overflow: "hidden", display: "flex", flexDirection: "column" }}>
       {children}
       {location.pathname === "/" ? <Attribution /> : null}
     </main>
